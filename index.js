7485932ba387a9dc8bb59e5ec21d3578476083d0
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-22 22:51:04 +0530

    code

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var isStudent = true;
-console.log(isStudent)
-isStudent = "abhbu";
-isStudent = 10
-console.log(isStudent);
+function greet(name){
+    return "hello,"+name;
+}
+let ans = greet("abhi");
+let ans2 = greet("hi");
+let ans3 = greet("bhi");
+
+console.log(ans);
+console.log(ans2);
+console.log(ans3);
