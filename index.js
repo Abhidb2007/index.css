5b010d34aa59393816642b6e4163df8886cd8b58
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-24 16:15:41 +0530

    code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("Start");
 
 setTimeout(() => {
-  console.log("This runs after 19 seconds");
+  console.log("hi BHI");
 }, 9000); // 9000 milliseconds = 19 seconds
 
 console.log("End");
