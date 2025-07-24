67106999db74f085e2bb76fae84034b2d03b215f
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-24 11:12:25 +0530

    code

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ function timeout(){
 console.log("hi ");
 }
 console.log("click  button");
-await setTimeout(timeout,10);
+setTimeout(timeout,10);
 console.log("welcome  loupe");
+let c=0;
+for(let i=0;i<1000000;i++){
+    c=c+1;
+
+}
+console.log("Expensive operation done");
