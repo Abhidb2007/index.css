4e53a9eb56ac2b668e0667afbdd7efecfb042e01
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-26 12:16:31 +0530

    code

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const fs = require("fs");
-
-fs.readFile("grocery.txt", "utf-8", function(err, data) {
-  if (err) {
-    console.log("File not found!");
-  } else {
-    console.log(data); 
-  }
-});
+setTimeoutPromisified(3000).then(callback)
