7bc8b280ff95504284f52d1bbaa330d19ebee10f
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-27 11:00:54 +0530

    code

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-const p = new Promise(async);
-function async(){
-    console.log("3 seconds have passed")
-}
-p.then(callback)
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
