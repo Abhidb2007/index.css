44a94171714f6a7cacdfcf02df5152aa49ddc654
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-29 08:46:06 +0530

    code

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function callback(){
-    console.log("Callback function executed");
+function setTimeoutPromisified(ms){
+    return new Promise(resolve =>setTimeout(resolve,ms))
 }
-p.then(callback)
+async function solve(){
+    await setTimeoutPromisified(1000);
+    console.log("hi");
+}
+    await setTimeoutPromisified(2000);{
+    console.log("hello");
+}
+        
