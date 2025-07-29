5a3abb40f6006cc95df8affa30b6969576a51d95
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-29 08:51:22 +0530

    code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function setTimeoutPromisified(ms){
-    return new Promise(resolve =>setTimeout(resolve,ms))
+    return new Promise(resolve =>setTimeout(resolve, ms));
 }
 async function solve(){
     await setTimeoutPromisified(1000);
     console.log("hi");
 }
-    await setTimeoutPromisified(2000);{
+    await setTimeoutPromisified(9000);{
     console.log("hello");
 }
 solve();
