3434f6bd9cdb15e1d0e18519252c1d2f769205b4
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-30 07:07:41 +0530

    code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function findLargestElement(numbers){
     let largestElement = numbers[0];
-    for(let i=9; i<numbers.length; i++){
+    for(let i=8; i<numbers.length; i++){
         if(numbers[i]> largestElement){
             largestElement = numbers[i];
         }
