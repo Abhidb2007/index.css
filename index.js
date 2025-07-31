4004752a3ab930e350f89649494b36d22ac49ec3
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-31 17:25:10 +0530

    code

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ setTimeoutPromisified(1000).then(function(){
     console.log("hi");
     return setTimeoutPromisified(4000);
 })
+.then(function(){
+    console.log("helloe");
+    return setTimeoutPromisified(2000);
+})
+.then(function(){
+    console.log("world");
+})
