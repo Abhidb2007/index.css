053f6a8a966761aaf18e0a6080afc8c871fdd154
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-31 17:22:33 +0530

    code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function setTimeoutPromisified(ms) {
     return new Promise(resolve =>setTimeout(resolve,ms));
 }
-setTimeoutPromisified(1000).then(function){
+setTimeoutPromisified(1000).then(function(){
     console.log("hi");
-    return setTimeoutPromisified(1000);
-}
+    return setTimeoutPromisified(4000);
+})
