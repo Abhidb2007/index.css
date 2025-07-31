9b775b9cf44a2e7f6f5317e200e2ffbf379558dc
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-31 16:42:38 +0530

    code

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 function callback(){
     console.log("Hello, World!");
 }
-setTimeoutPromisified(1000).then(callback).catch(console.error);
-function setTimeoutPromisified(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+setTimeoutPromisified(1000)
