e1b61e029326fd2c415a53e89560e5aa2f5999bb
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-06 16:59:24 +0530

    create todo

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const todos = [
   { id: 1, title: "Learn React", completed: false },
   { id: 2, title: "Build a ToDo App", completed: false },
 ];
+function createToDoComponent(todo) {
+  const li = document.createElement("li");
+  li.textContent = todo.title;
+  li.className = todo.completed ? "completed" : "";
+  return li;
+}
