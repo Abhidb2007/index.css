c8b877102e0092612052394aafdec0af07da6e18
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-12 07:10:00 +0530

    file change text

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const fs = require("fs");
 function main(fileName){
   fs.readFile(fileName,"utf-8", function(err, data){
     let total=0;
-    for (let i=0;i<data.length;i++){
+    if (err) {
+      console.error("Error reading file:", err.message);
+      return;
+    }
+    for (let i = 0; i < data.length; i++){
       if(data[i]===" "){
         total++;
       }
     }
-    console.log(total+1);
+    console.log(total + 1);
   })
 }
 main("a.text");
