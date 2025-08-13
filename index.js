c9a1a237fdc31cc0a287b05f4c369efeae6a34fd
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-13 06:49:09 +0530

    body

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
+const path = require("path");
 
 function main(fileName) {
-  fs.readFile(fileName, "utf-8", function (err, data) {
+  const filePath = path.join(__dirname, fileName); // Always read from same folder as index.js
+
+  fs.readFile(filePath, "utf-8", function (err, data) {
     if (err) {
       console.error("Error reading file:", err);
       return;
     }
     // Split the text by whitespace and filter out empty strings
     const words = data.trim().split(/\s+/).filter(Boolean);
-    console.log(words.length);
+    console.log("Word count:", words.length);
   });
 }
 
-main("a.text");
+// Make sure you actually have a.txt in the same folder
+main("a.txt");
