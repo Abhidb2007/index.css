d0924c5515acdbbbd2f6cbd16806a0a1f4e42a10
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-14 07:16:01 +0530

    code

--- App.jsx
+++ App.jsx
@@ -1,10 +1,19 @@
-import *./App.css*;
-export default function App(){
-    return <div>
-    <b>
-    <h2>
-    hi abhi
-    </h2>
-    </b>
-    </div>
+import { useState } from "react";
+import "./App.css";
+function Button(props){
+  return <button onClick={props.onClickHandler}>Counter {count}</button>;
 }
+
+export default function App() {
+  const [count, setcount] = useState(0);
+  function onClickHandler() {
+    setcount(count + 1);
+  }
+  return (
+    <div>
+      <h2>Running on: localhost</h2>
+      <Button onClickHandler={onClickHandler} count={count} />
+    </div>
+  );
+}
+
