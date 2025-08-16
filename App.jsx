b4dec4ff980bfbe6824f63208ce8bf9b7d975508
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-16 06:41:35 +0530

    setcount

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-function app(){
+function App(){
   return <div>
     <h1>hi</h1>
     <counter></counter>
@@ -7,12 +7,7 @@ function app(){
 }
 function Counter(){ 
   const[count, setCount]=useState(0);
-function increaseCount(){
-    setCount(count+1);
-}  
-function decreaseCount(){
-  setCount(count-1);
-}
+
   return <div>
     <h1 id="text">{count}
     </h1>
