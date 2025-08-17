f06d8e80a8756ca49622290eb91a07f913daa4af
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-17 14:22:44 +0530

    simple page created

--- App.jsx
+++ App.jsx
@@ -1,37 +1,49 @@
-import{useState, useCount, useEffect} from "react";
-export default function App(){
-  const[count, setCount]=useState(0);
-  const[count2, setCount2]=useState(0);
+import React from "react";
+
+const style = {
+  width: 200,
+  backgroundColor: "white",
+  borderRadius: 10,
+  borderWidth: 1,
+  display: "flex", // corrected 'displays' to 'display'
+  flexDirection: "column",
+  alignItems: "center",
+  padding: 10
+};
+
+function PostComponent() {
+  return (
+    <div style={style}>
+      <img
+        src="https://static.vecteezy.com/system/resources/thumbnails/026/525/162/small_2x/lion-animal-isolated-photo.jpg"
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 20,
+          objectFit: "cover"
+        }}
+        alt="Lion"
+      />
+      <div style={{ fontSize: 20, margin: 10 }}>
+        <b>100xdevs</b>
+        <div>hi there1</div>
+        <div>hi there2</div>
+      </div>
+    </div>
+  );
 }
-  function increase(){
-    setCount(count=count+1);
-  }
-  function decrease(){
-    setCount(count=count+2);
-  }
-  return <div>
-    <Counter count={count} count2={count2}></Counter>
-    <button onClick={increase}>home</button>
-    <button onClick={decrease}>afsd</button>
-  </div>
-  function Counter(props){
 
-  
-  useEffect (function(){
-    console.log("mount");
-    return function(){
-      console.log("unmount");
-    }
-
-  },[]);
-  useEffect(function () {
-    console.log("count has changed")
-    return function (){
-      console.log("cleanup inside the second effect")
-    }
-  },[props.count]);
-  return <div>
-    counter1{count1}<br/>
-    counter2{count2}<br/>
-  </div>  
+export default function App() {
+  return (
+    <div style={{ backgroundColor: "red", padding: 20 }}>
+      <div style={{ display: "flex", gap: 20 }}>
+        <div>
+          <PostComponent />
+          <br />
+          <PostComponent />
+          <br />
+        </div>
+      </div>
+    </div>
+  );
 }
