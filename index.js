c56a8ef390f589c8d7cd3bce8963d715604d9326
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-17 20:11:45 +0530

    code

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ function App(){
     return <div style={{display:"flex"}}>
         <Card innerContent={<div style={{color:"green",borderRadius:5}}>
             i am learn <br/>
-            <input type={"text"}>
+            <input type={"text"}
             </input>
 
         </div>}/>
         <Card innerContent={"hi there"}></Card>
     </div>
-
-
+}
+function Card {(innerContent)}{
+    return <div style={{color:white,borderRadius:4,backGround:yellow,padding:10}}>
+        {innerContent}
+    </div>
 }
