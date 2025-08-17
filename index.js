5eabe85e2210f211b2b4b82e5ebcb4b03ab55502
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-17 23:17:19 +0530

    class component

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-function App(){
-    return <div style={{display:"flex"}}>
-        <Card innerContent={<div style={{color:"green",borderRadius:5}}>
-            i am learn <br/>
-            <input type={"text"}
-            </input>
-
-        </div>}/>
-        <Card innerContent={"hi there"}></Card>
-    </div>
-}
-function Card {(innerContent)}{
-    return <div style={{color:white,borderRadius:4,backGround:yellow,padding:10}}>
-        {innerContent}
-    </div>
+import React,{component} from 'react';
+class classCounter extends component{
+    state={count:0};
+    increament(){
+        this.setState({count:this.state.count+1});
+    };
+    render(){
+        return(
+            <div>
+                <p>Count:{this.state.count}</p>
+                <button onClick={this.state.count}></button>
+            </div>
+        )
+    }
 }
