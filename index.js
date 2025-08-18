bb2b0fa71afe022e833b312c1e96dbe16beeac63
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-18 23:41:49 +0530

    code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React,{component} from 'react';
 class classCounter extends component{
-    state={count:5};
+    state={count:6};
     increament(){
         this.setState({count:this.state.count+1});
     };
